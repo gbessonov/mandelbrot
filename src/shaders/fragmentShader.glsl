303c4da7ac65c92c9a01285cf44bdb78f6935360
Gregory <greg.bessonov@gmail.com>
2025-06-18 12:09:31 +0200

    shader prep

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -8,6 +8,30 @@ uniform float u_zoom;
 
 out vec4 outColor;
 
+float hp(float value) {
+    return value;
+}
+
+float hp_sum(float a, float b) {
+    return a + b;
+}
+
+float hp_sub(float a, float b) {
+    return a - b;
+}
+
+float hp_mul(float a, float b) {
+    return a * b;
+}
+
+float hp_scale(float k, float value) {
+    return k * value;
+}
+
+float hp_square(float a) {
+    return a * a;
+}
+
 vec4 getColor(float t) {
     if (t > 0.9999999) return vec4(0.1, 0.1, 0.1, 1.0);
     vec3 a = vec3(0.1, 0.1, 0.3);
@@ -17,17 +41,27 @@ vec4 getColor(float t) {
 
 void main() {
     vec2 c = (gl_FragCoord.xy - 0.5 * u_resolution - u_center_high - u_center_low) / u_zoom;
-    vec2 z = vec2(0.0);
+
+    float c_re = c.x;
+    float c_im = c.y;
+
+    float z_re = 0.0;
+    float z_im = 0.0;
+
     const int maxIterations = 200;
     int iterations = 0;
     for (int i = 0; i < maxIterations; i++) {
-        if (dot(z, z) > 4.0) break;
-        z = vec2(
-        z.x * z.x - z.y * z.y + c.x,
-        2.0 * z.x * z.y + c.y
-        );
+        float z_re2 = hp_square(z_re);
+        float z_im2 = hp_square(z_im);
+        if ((z_re2 + z_im2) > 4.0) {
+            break;
+        }
+        float z_re_old = z_re;
+        z_re = hp_sum(hp_sub(z_re2, z_im2), c_re);
+        z_im = hp_sum(hp_scale(2.0, hp_mul(z_re_old, z_im)), c_im);
         iterations++;
     }
+
     float t = float(iterations) / float(maxIterations);
     outColor = getColor(t);
 }
